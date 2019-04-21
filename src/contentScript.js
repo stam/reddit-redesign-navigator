22de0f73deb38e19f59954c25bcda18180f068c0
Jasper Stam <jasper.stam@philips.com>
2019-04-21 17:25:26 +0200

    Structure behavior into classes, detect basic navigation

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,37 +1,104 @@
+class Post {
+  constructor(element) {
+    this.element = element;
+  }
+
+  highlight(color = "hotpink") {
+    this.element.style.background = color;
+  }
+}
+
+class DomManager {
+  static findActivePost(container) {
+    const element = document.activeElement;
+
+    if (DomManager.isElementPost(element)) {
+      return new Post(element);
+    }
+
+    return null;
+  }
+
+  static isElementPost(element) {
+    return element.classList.contains("Post");
+  }
+
+  static findSiblingPost(element, direction) {
+    console.log("el", element.classList);
+    if (!DomManager.isElementPost(element)) {
+      throw new Error("Target element is not a Post", element);
+    }
+
+    const parentWithSiblings = element.parentElement.parentElement;
+    const uncle =
+      direction === "UP"
+        ? parentWithSiblings.nextElementSibling
+        : parentWithSiblings.previousElementSibling;
+    const target = uncle.firstElementChild.firstElementChild;
+
+    // TODO check if target is visible (could be an ad that's hidden)
+    return new Post(target);
+  }
+}
+
 class Navigator {
   constructor(selector) {
     this.element = document.querySelector(selector);
     if (!this.element) {
-      throw new Error(`Redesign navigator failed to construct, element with selector ${selector} not found.`);
+      throw new Error(
+        `Redesign navigator failed to construct, element with selector ${selector} not found.`
+      );
     }
     this.bind();
   }
 
-  handleKeydown = (event) => {
+  handleNavigation(direction, event) {
+    const activePost = DomManager.findActivePost(this.element);
+
+    if (activePost) {
+      activePost.highlight();
+    }
+
+    const previousPost = DomManager.findSiblingPost(
+      activePost.element,
+      direction
+    );
+
+    if (previousPost) {
+      previousPost.highlight("blue");
+    }
+    // if previousPost is expanded, scroll activePost into view, and expand.
+  }
+
+  handleKeydown = event => {
     switch (event.keyCode) {
       // After J press, make it highlight next post and not the first one if already opened one
       // When in X mode, also open next preview and scroll it into view
       case 74: // J
-        console.log('next');
+        setTimeout(() => {
+          this.handleNavigation("DOWN", event);
+        });
         break;
       case 75: // K
-        console.log('previous');
+        setTimeout(() => {
+          this.handleNavigation("UP", event);
+        });
         break;
       // Make X stateful
       case 88: // X
-        console.log('show preview');
+        console.log("show preview");
         break;
     }
-  }
+  };
 
   bind() {
-    this.element.addEventListener('keydown', this.handleKeydown);
+    this.element.addEventListener("keydown", this.handleKeydown.bind(this));
   }
 
   unbind() {
-    this.element.removeEventListener('keydown', this.handleKeydown);
+    this.element.removeEventListener("keydown", this.handleKeydown.bind(this));
   }
 }
 
 // TODO add and destroy listeners when switching pages
-const redditRedesignNavigator = new Navigator('#SHORTCUT_FOCUSABLE_DIV');
+const redditRedesignNavigator = new Navigator("#SHORTCUT_FOCUSABLE_DIV");
