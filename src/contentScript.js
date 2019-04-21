4faa9dcdd4b4e961548cc6d1c61b54d5a23f3773
Jasper Stam <jasper.stam@philips.com>
2019-04-21 15:37:36 +0200

    Bundle event binding for easier cleanup

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,34 +1,37 @@
-console.log(
-  '%c Loaded extension on reddit',
-  'background: red; color: yellow; font-size: x-large'
-);
+class Navigator {
+  constructor(selector) {
+    this.element = document.querySelector(selector);
+    if (!this.element) {
+      throw new Error(`Redesign navigator failed to construct, element with selector ${selector} not found.`);
+    }
+    this.bind();
+  }
 
-
-function handleKeydown(event) {
+  handleKeydown = (event) => {
     switch (event.keyCode) {
-        // After J press, make it highlight next post and not the first one if already opened one
-        // When in X mode, also open next preview and scroll it into view
-        case 74: // J
-            console.log('next');
-            break;
-        case 75: // K
-            console.log('previous');
-            break;
-        // Make X stateful
-        case 88: // X
-            console.log('show preview');
-            break;
+      // After J press, make it highlight next post and not the first one if already opened one
+      // When in X mode, also open next preview and scroll it into view
+      case 74: // J
+        console.log('next');
+        break;
+      case 75: // K
+        console.log('previous');
+        break;
+      // Make X stateful
+      case 88: // X
+        console.log('show preview');
+        break;
     }
-}
+  }
 
-function startListening() {
-    const postContainer = document.querySelector('#SHORTCUT_FOCUSABLE_DIV');
+  bind() {
+    this.element.addEventListener('keydown', this.handleKeydown);
+  }
 
-    if (!postContainer) {
-        return;
-    }
-    postContainer.addEventListener('keydown', handleKeydown);
+  unbind() {
+    this.element.removeEventListener('keydown', this.handleKeydown);
+  }
 }
 
 // TODO add and destroy listeners when switching pages
-startListening();
+const redditRedesignNavigator = new Navigator('#SHORTCUT_FOCUSABLE_DIV');
